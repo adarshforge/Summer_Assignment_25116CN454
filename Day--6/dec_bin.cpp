f49f1fd16da405e17111825a95d275c60767ce56
Adarsh <adarsh12srivastava@gmail.com>
2026-06-04 23:16:24 +0530

    Day--6

---
+++ Day--6/dec_bin.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n, binary[32], i = 0;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    while (n > 0) {
+        binary[i] = n % 2;
+        n /= 2;
+        i++;
+    }
+
+    cout << "Binary representation: ";
+    for (int j = i - 1; j >= 0; j--) {
+        cout << binary[j];
+    }
+
+    return 0;
+}
---
+++ Day--6/exp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, n;
+    long long result = 1;
+
+    cout << "Enter x and n: ";
+    cin >> x >> n;
+
+    for (int i = 1; i <= n; i++) {
+        result *= x;
+    }
+
+    cout << x << "^" << n << " = " << result;
+
+    return 0;
+}
