dfdcd1e05842b85b89fdb8ad9c9c3edf1dc9368c
Adarsh <adarsh12srivastava@gmail.com>
2026-06-14 11:18:39 +0530

    Day--07 of week2 uploaded

---
+++ WEEK-2/Day--07/freq.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, key, count = 0;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Enter element to find frequency: ";
+    cin >> key;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == key)
+        {
+            count++;
+        }
+    }
+
+    cout << "Frequency of " << key << " = " << count;
+
+    return 0;
+}
