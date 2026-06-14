99c2718b3dfecae547c373bbac2cddc4c2a52198
Adarsh <adarsh12srivastava@gmail.com>
2026-06-14 23:04:16 +0530

    Day--01 week3 uploaded

---
+++ WEEK-3/Day--02/missing_number.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter value of n: ";
+    cin >> n;
+
+    int arr[n - 1];
+
+    cout << "Enter " << n - 1 << " elements: ";
+    for (int i = 0; i < n - 1; i++) {
+        cin >> arr[i];
+    }
+
+    int expectedSum = n * (n + 1) / 2;
+    int actualSum = 0;
+
+    for (int i = 0; i < n - 1; i++) {
+        actualSum += arr[i];
+    }
+
+    cout << "Missing number = " << expectedSum - actualSum;
+
+    return 0;
+}
